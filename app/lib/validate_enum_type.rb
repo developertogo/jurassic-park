6714e2090045d54ee04b442dea024efe122cd04c
Carlos Hung <developertogo@gmail.com>
2023-01-23 20:09:02 -0800

    Commented out unused code
    To be removed

--- app/lib/validate_enum_type.rb
+++ app/lib/validate_enum_type.rb
@@ -1,8 +1,11 @@
-# override ActiveRecord::Enum::EnumType's assert_valid_value
-class ValidatableEnumType < ActiveRecord::Enum::EnumType
-  # return value supressing <ArgumentError> found in
-  # https://github.com/rails/rails/blob/main/activerecord/lib/active_record/enum.rb#L171
-  def assert_valid_value(value)
-    value
-  end
-end
+# NOTE: To be removed. Commented out for now.
+#       Failed in GitHub build with this error:
+#         Expected file #{file} to define constant #{cpath}, but didn't
+# # override ActiveRecord::Enum::EnumType's assert_valid_value
+# class ValidatableEnumType < ActiveRecord::Enum::EnumType
+#   # return value supressing <ArgumentError> found in
+#   # https://github.com/rails/rails/blob/main/activerecord/lib/active_record/enum.rb#L171
+#   def assert_valid_value(value)
+#     value
+#   end
+# end
