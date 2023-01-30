a194e21ab6c4190bbfe67007d4bd559fb664bfa1
Carlos Hung <developertogo@gmail.com>
2023-01-29 19:34:41 -0800

    Update comment about `counter_cache` on dinosaur model

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -3,10 +3,7 @@
 require './app/lib/park'
 
 class Dinosaur < ApplicationRecord
-  # NOTE: attempted to use counter_cache but cage.dinosaurs_count becomes -1
-  # When moving dinosaur from the "from" cage containing just this dinosaur.
-  # Expected the count = 0, but not -1; even from_cage.dinosaurs.size returns -1
-  # belongs_to :cage #, counter_cache: true
+  # NOTE: Could not use counter_cache, need association callbacks to be fired
   belongs_to :cage, optional: true
 
   before_save :update_diet
